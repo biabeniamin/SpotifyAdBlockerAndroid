b5648e80e0e4d77fd98c1c562342a7f262b6ac21
Beniamin Bia <biabeniamin@outlook.com>
2020-03-03 19:55:52 +0200

    create spotify service

---
+++ SpotifyAdBlocker/app/src/main/java/com/example/spotifyadblocker/NotificationListener.java
@@ -0,0 +1,36 @@
+package com.example.spotifyadblocker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+public class NotificationListener  extends NotificationListenerService {
+    private String TAG = this.getClass().getSimpleName();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        Log.i(TAG,"**********  onNotificationPosted");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG,"********** onNOtificationRemoved");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText +"\t" + sbn.getPackageName());
+    }
+}
